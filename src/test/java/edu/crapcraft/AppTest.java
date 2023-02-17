e0996f068a6e96e105c624c3f01b9df96854215f
dfleta <gelpiorama@gmail.com>
2023-02-17 11:42:54 +0100

    build(pom.xml): Configurar un pom minimo

---
+++ src/test/java/edu/crapcraft/AppTest.java
@@ -0,0 +1,14 @@
+package edu.crapcraft;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest 
+{
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
